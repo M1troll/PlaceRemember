4277848ea1ed91fd1cd4d25b5fec745fac9886b6
M1troll <oboleninoff.anton@yandex.ru>
2022-04-20 13:00:37 +0700

    Create a project
    A project was created, an application was added, and basic configuration was performed.

---
+++ PlaceRemember/PlaceRemember/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for PlaceRemember project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PlaceRemember.settings')
+
+application = get_asgi_application()
---
+++ PlaceRemember/impression_storage/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'impression_storage'
+urlpatterns = [
+    path('', views.index, name='index'),
+]
