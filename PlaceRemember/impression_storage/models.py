ad195cd6fc2d1b5ae9fd75a32d0ec70a55e1dc8e
M1troll <oboleninoff.anton@yandex.ru>
2022-04-20 23:35:22 +0700

    Create models

--- PlaceRemember/impression_storage/models.py
+++ PlaceRemember/impression_storage/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, verbose_name="Никнейм")
+    image = models.ImageField(verbose_name="Фото")
+
+
+class Impression(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Название места")
+    description = models.TextField(verbose_name="Описание")
+
+    lon = models.FloatField(verbose_name="Долгота")
+    lat = models.FloatField(verbose_name="Широта")
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
