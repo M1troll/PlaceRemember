8c68bf61e222f4d3734621e716415c87cf18557e
M1troll <oboleninoff.anton@yandex.ru>
2022-04-21 23:17:16 +0700

    Change models

--- PlaceRemember/impression_storage/models.py
+++ PlaceRemember/impression_storage/models.py
@@ -1,17 +1,30 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-class User(AbstractUser):
-    username = models.CharField(max_length=50, verbose_name="Никнейм")
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(verbose_name="Фото")
 
     def __str__(self):
-        return f"{self.username}"
+        return f"{self.user.username}"
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 class Impression(models.Model):
@@ -22,7 +35,7 @@ class Impression(models.Model):
     lat = models.FloatField(verbose_name="Широта")
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return f"{self.title}"
 
