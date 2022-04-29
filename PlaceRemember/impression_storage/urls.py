e1b856558f78ba133d4f48c1e2dc35479c9009db
M1troll <oboleninoff.anton@yandex.ru>
2022-04-29 19:57:58 +0700

    Add impression edit  page

--- PlaceRemember/impression_storage/urls.py
+++ PlaceRemember/impression_storage/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.contrib.auth.views import LogoutView
 
 from .views import *
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('delete/<int:pk>', DeleteImpressionView.as_view(), name='delete_impression'),
     path('storage/', StorageView.as_view(), name='storage'),
-    path('storage/<int:pk>', StorageView.as_view(), name='storage_edit'),
+    path('storage/<int:pk>/', EditStorageView.as_view(), name='storage_edit'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    # re_path(r'^.*', IndexView.as_view(), name='index'),
 ]
--- PlaceRemember/impression_storage/views.py
+++ PlaceRemember/impression_storage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Impression
 from .forms import ImpressionForm
@@ -12,17 +13,20 @@ class IndexView(View):
         return render(request, 'impression_storage/index.html', self.context)
 
 
-class StorageView(View):
+class StorageView(LoginRequiredMixin, View):
+    login_url = '/'
+    redirect_field_name = '/'
+
     context = {
         'form': ImpressionForm(),
     }
 
-    def get(self, request, pk=''):
+    def get(self, request):
         self.context["impressions"] = Impression.objects.filter(author=request.user)
         return render(request, 'impression_storage/storage.html', self.context)
 
     @staticmethod
-    def post(request, pk=''):
+    def post(request):
         form = ImpressionForm(request.POST)
         if form.is_valid():
             impression = form.save(commit=False)
@@ -32,7 +36,42 @@ class StorageView(View):
         return redirect('/storage/')
 
 
-class DeleteImpressionView(View):
+class EditStorageView(LoginRequiredMixin, View):
+    login_url = '/'
+    redirect_field_name = '/'
+
+    context = {}
+
+    def get(self, request, pk):
+        try:
+            impression = Impression.objects.get(pk=pk)
+        except Impression.DoesNotExist:
+            return render(request, 'impression_storage/storage.html', self.context)
+
+        self.context["impression_pk"] = impression.pk
+        self.context["form"] = ImpressionForm(instance=impression)
+
+        return render(request, 'impression_storage/edit_storage.html', self.context)
+
+    def post(self, request, pk):
+        try:
+            impression = Impression.objects.get(pk=pk)
+        except Impression.DoesNotExist:
+            return render(request, 'impression_storage/storage.html', self.context)
+
+        form = ImpressionForm(request.POST, request.FILES)
+        if form.is_valid():
+            for key, value in form.cleaned_data.items():
+                setattr(impression, key, value)
+            impression.save()
+
+        return redirect('/storage/')
+
+
+class DeleteImpressionView(LoginRequiredMixin, View):
+    login_url = '/'
+    redirect_field_name = '/'
+
     context = {}
 
     @staticmethod
